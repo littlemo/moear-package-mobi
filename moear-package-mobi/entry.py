10c53e24a0d4070965dffa714705b1da35a652b4
Moore.Huang <moore@moorehy.com>
2018-03-08 16:40:17 +0800

    F: 创建包结构，以及entry中的接口类定义

---
+++ moear-package-mobi/entry.py
@@ -0,0 +1,12 @@
+from moear_api_common import base
+
+
+class Mobi(base.PackageBase):
+    """
+    Mobi打包
+    ========
+
+    用以实现mobi打包工具
+    """
+    def generate(self, data, *args, **kwargs):
+        pass
