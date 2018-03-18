45cdfe0d0cf75982cfcc6bee3f411cbf6eaa2d63
Moore.Huang <moore@moorehy.com>
2018-03-18 12:34:02 +0800

    F: 实现定制化的图片pipeline，将图片文件输出到Spider类中指定的路径下

--- moear_package_mobi/pipelines.py
+++ moear_package_mobi/pipelines.py
@@ -4,8 +4,21 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+
+from scrapy.pipelines.images import ImagesPipeline
 
 
-class MoearPackageMobiPipeline(object):
-    def process_item(self, item, spider):
-        return item
+class MoEarImagesPipeline(ImagesPipeline):
+    """
+    定制ImagesPipeline，实现图片保存路径的自定义
+    """
+
+    def file_path(self, request, response=None, info=None):
+        url = super(MoEarImagesPipeline, self).file_path(
+            request, response=response, info=info)
+        url_new = os.path.join(
+            info.spider.tmpdir, 'images', url.split('/')[-1])
+        info.spider.logger.debug(
+            '保存图片：{} | {} | {}'.format(response, request, url_new))
+        return url
