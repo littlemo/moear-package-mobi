6dc25b184215f9f4a0dd64adab49880d791427e8
Moore.Huang <moore@moorehy.com>
2018-03-18 21:31:52 +0800

    F: 增加图片处理pipeline设置

--- moear_package_mobi/settings.py
+++ moear_package_mobi/settings.py
@@ -66,9 +66,9 @@ COOKIES_ENABLED = False
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'moear_package_mobi.pipelines.MoearPackageMobiPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'moear_package_mobi.pipelines.MoEarImagesPipeline': 109,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
