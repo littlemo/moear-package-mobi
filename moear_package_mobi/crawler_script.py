e4c337159f148c4e90c9522926470650965445ec
Moore.Huang <moore@moorehy.com>
2018-03-19 09:27:38 +0800

    F: 实现外部调用时的crawler脚本类

---
+++ moear_package_mobi/crawler_script.py
@@ -0,0 +1,22 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from billiard import Process
+from . import settings as config
+
+
+class CrawlerScript():
+
+    def __init__(self):
+        settings = Settings()
+        settings.setmodule(config)
+        self.crawler = CrawlerProcess(settings)
+
+    def _crawl(self, *args, **kwargs):
+        self.crawler.crawl('mobi', *args, **kwargs)
+        self.crawler.start()
+        self.crawler.stop()
+
+    def crawl(self, *args, **kwargs):
+        p = Process(target=self._crawl, args=args, kwargs=kwargs)
+        p.start()
+        p.join()
