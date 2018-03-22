aa8db6db6b95a23f2da67835a7dc7325e9576b74
Moore.Huang <moore@moorehy.com>
2018-03-22 14:14:06 +0800

    F: 根据重构后的图片过滤方法，更新单元测试

--- tests/test_mobi.py
+++ tests/test_mobi.py
@@ -30,24 +30,21 @@ class TestSpiderMobiMethods(unittest.TestCase):
 
     def test_000_filter_images_urls_with_list_filter(self):
         """测试传入列表过滤器到图片链接列表过滤器方法中"""
-        rc = mobi.MobiSpider.filter_images_urls(
+        rc, rc_removed = mobi.MobiSpider.filter_images_urls(
             self.image_urls, self.image_filter)
         log.debug(rc)
-        self.assertNotIn(
-            'https://pic3.zhimg.com/v2-46e7a28b89f7e5351355af36c7221cee.jpg',
-            rc)
-        self.assertNotIn(
-            'https://pic3.zhimg.com/equation?tex=f7e5351355af36c7221cee.jpg',
-            rc)
+        self.assertNotIn(self.image_urls[1], rc)
+        self.assertNotIn(self.image_urls[2], rc)
+        self.assertIn(self.image_urls[1], rc_removed)
+        self.assertIn(self.image_urls[2], rc_removed)
 
     def test_001_filter_images_urls_with_str_filter(self):
         """测试传入单字串过滤器参数到图片列表过滤器方法中"""
-        rc = mobi.MobiSpider.filter_images_urls(
+        rc, rc_removed = mobi.MobiSpider.filter_images_urls(
             self.image_urls, self.image_filter[1])
         log.debug(rc)
-        self.assertNotIn(
-            'https://pic3.zhimg.com/equation?tex=f7e5351355af36c7221cee.jpg',
-            rc)
+        self.assertNotIn(self.image_urls[2], rc)
+        self.assertIn(self.image_urls[2], rc_removed)
 
     def test_002_filter_images_urls_with_invalid_filter(self):
         """测试传入非法类型的过滤器参数到图片列表过滤器中"""
@@ -59,14 +56,16 @@ class TestSpiderMobiMethods(unittest.TestCase):
 
     def test_003_filter_images_urls_with_null_str_filter(self):
         """测试传入空字符串类型的过滤器参数到图片列表过滤器中"""
-        rc = mobi.MobiSpider.filter_images_urls(
+        rc, rc_removed = mobi.MobiSpider.filter_images_urls(
             self.image_urls, '')
         log.debug(rc)
         self.assertEqual(rc, self.image_urls)
+        self.assertEqual(rc_removed, [])
 
     def test_004_filter_images_urls_with_null_list_filter(self):
         """测试传入空字符串类型的过滤器参数到图片列表过滤器中"""
-        rc = mobi.MobiSpider.filter_images_urls(
+        rc, rc_removed = mobi.MobiSpider.filter_images_urls(
             self.image_urls, [])
         log.debug(rc)
         self.assertEqual(rc, self.image_urls)
+        self.assertEqual(rc_removed, [])
